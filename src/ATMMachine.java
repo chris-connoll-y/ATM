71376e88ca077c6b27ed898f34f9b1553c083fea
chris-connoll-y <89544126+chris-connoll-y@users.noreply.github.com>
2021-08-27 09:09:46 -0700

    Initial commit

---
+++ src/ATMMachine.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ATMMachine {
+	private HashMap Bank;
+	public ATMMachine () {
+		
+	}
+	
+	public void deposit (String ID, double amount) {
+		if (!Bank.containsKey(ID)) {
+			Bank.put(ID, amount);
+		}
+		else {
+			double currentAmount = (double)Bank.get(ID);
+			double newAmount = amount + currentAmount;
+			Bank.replace (ID, newAmount);
+		}
+	}
+}
